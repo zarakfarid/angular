20c8dc305f56159f0ce408000f124f7610238532
Maciej Malecki <maciej.malecki@capgemini.com>
2021-10-27 15:28:36 +0200

    dom tests

--- src/app/books/components/books-details/books-details.component.spec.ts
+++ src/app/books/components/books-details/books-details.component.spec.ts
@@ -1,72 +1,172 @@
-import { BooksDetailsComponent } from './books-details.component';
+import {BooksDetailsComponent} from './books-details.component';
 import {Book} from "../../model/book";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
-fdescribe('BooksDetailsComponent', () => {
-
-  describe("[class]", () => {
+describe('BooksDetailsComponent', () => {
 
     let component: BooksDetailsComponent;
     let aBook: Book;
 
     beforeEach(() => {
-      component = new BooksDetailsComponent();
-      aBook = {
-        id: null,
-        title: "Solaris",
-        author: "Stanisław Lem",
-        year: 1960
-      };
+        aBook = {
+            id: null,
+            title: "Solaris",
+            author: "Stanisław Lem",
+            year: 1960
+        };
     });
 
-    it("intially contains no book", () => {
-      expect(component.book).toBeNull();
+    describe("[class]", () => {
+
+
+        beforeEach(() => {
+            component = new BooksDetailsComponent();
+        });
+
+        it("intially contains no book", () => {
+            expect(component.book).toBeNull();
+        });
+
+        it("allows to set a book", () => {
+            component.book = aBook;
+            expect(component.book).toEqual(aBook);
+        });
+
+        it("makes a copy of an original book object", () => {
+            component.book = aBook;
+            expect(component.book).not.toBe(aBook);
+        });
+
+        it("allows to modify the book inside", () => {
+            component.book = aBook;
+            component.book.title = "Ubik";
+            expect(component.book).not.toEqual(aBook);
+            expect(component.book).toEqual({...aBook, title: "Ubik"});
+        });
+
+        it("allows to revert modified book to the original value", () => {
+            // given
+            component.book = aBook;
+            // when
+            component.book.title = "Ubik";
+            // then
+            expect(component.book).not.toEqual(aBook);
+            // when
+            component.revert();
+            // then
+            expect(component.book).toEqual(aBook);
+        });
+
+        it("emits the current value when save is clicked", () => {
+            component.book = aBook;
+            const newTitle = "Ubik";
+            const newAuthor = "Phillip K Dick";
+            component.book.title = newTitle;
+            component.book.author = newAuthor;
+            let bookUpdated: Book | null = null;
+            component.bookUpdated.subscribe(value => {
+                bookUpdated = value;
+            });
+            // when
+            component.save();
+            // then
+            expect(bookUpdated).not.toBeNull();
+            expect(bookUpdated!).toEqual({...aBook, title: newTitle, author: newAuthor});
+        });
     });
 
-    it("allows to set a book", () => {
-      component.book = aBook;
-      expect(component.book).toEqual(aBook);
-    });
+    describe("[DOM]", () => {
 
-    it("makes a copy of an original book object", () => {
-      component.book = aBook;
-      expect(component.book).not.toBe(aBook);
-    });
+        let fixture: ComponentFixture<BooksDetailsComponent>;
+        let nativeElement: HTMLElement;
 
-    it("allows to modify the book inside", () => {
-      component.book = aBook;
-      component.book.title = "Ubik";
-      expect(component.book).not.toEqual(aBook);
-      expect(component.book).toEqual({...aBook, title: "Ubik"});
-    });
+        const getHTMLInput = (selector: string) => (nativeElement.querySelector(selector) as HTMLInputElement);
+        const getTitleInput = () => getHTMLInput("#title");
+        const getAuthorInput = () => getHTMLInput("#author");
+        const getYearInput = () => getHTMLInput("#year");
+        const editInput = (element: HTMLInputElement, value: string) => {
+            element.value = value;
+            element.dispatchEvent(new Event("input"));
+        }
+        const clickSave = () => {
+            nativeElement.querySelector("button.app-save-button")?.dispatchEvent(new Event("click"));
+        }
 
-    it("allows to revert modified book to the original value", () => {
-      // given
-      component.book = aBook;
-      // when
-      component.book.title = "Ubik";
-      // then
-      expect(component.book).not.toEqual(aBook);
-      // when
-      component.revert();
-      // then
-      expect(component.book).toEqual(aBook);
-    });
+        beforeEach(async () => {
+            await TestBed.configureTestingModule({
+                declarations: [BooksDetailsComponent],
+                imports: [CommonModule, FormsModule]
+            }).compileComponents();
+        });
 
-    it("emits the current value when save is clicked", () => {
-      component.book = aBook;
-      const newTitle = "Ubik";
-      const newAuthor = "Phillip K Dick";
-      component.book.title = newTitle;
-      component.book.author = newAuthor;
-      let bookUpdated: Book | null = null;
-      component.bookUpdated.subscribe(value => {
-        bookUpdated = value;
-      });
-      // when
-      component.save();
-      // then
-      expect(bookUpdated).not.toBeNull();
-      expect(bookUpdated!).toEqual({...aBook, title: newTitle, author: newAuthor});
+        beforeEach(() => {
+            fixture = TestBed.createComponent(BooksDetailsComponent);
+            component = fixture.componentInstance;
+            nativeElement = fixture.nativeElement;
+            fixture.detectChanges();
+        });
+
+        it("for no book we don't see a form, but rather a message", () => {
+            expect(nativeElement.querySelector("p")?.innerText).toBe("Please select a book.");
+        });
+
+        it("once the book is selected, we see a form", async () => {
+            component.book = aBook;
+            fixture.detectChanges();
+            expect(nativeElement.querySelector("p")).toBeFalsy();
+            expect(nativeElement.querySelector("h2")).toBeTruthy();
+            expect(nativeElement.querySelector("h2")?.innerText).toBe("Book details");
+            await fixture.whenStable(); // return fixture.whenStable(() => { ...
+            expect(getTitleInput().value).toBe(aBook.title);
+            expect(getAuthorInput().value).toBe(aBook.author);
+            expect(getYearInput().value).toBe(aBook.year.toString());
+            // ... });
+        });
+
+        it("once the book is edited, its value is propagated ti the model", () => {
+            component.book = aBook;
+            fixture.detectChanges();
+
+            const newValues = {
+                author: "Phillip K Dick",
+                title: "Ubik",
+                year: 1970
+            };
+
+            editInput(getTitleInput(), newValues.title);
+            editInput(getAuthorInput(), newValues.author);
+            editInput(getYearInput(), `${newValues.year}`);
+
+            expect(component.book.title).toBe(newValues.title);
+            expect(component.book.author).toBe(newValues.author);
+            expect(component.book.year).toBe(newValues.year);
+        });
+
+        it("once save button is clicked, data is emitted", () => {
+            component.book = aBook;
+            fixture.detectChanges();
+
+            let bookReceived: Book | null = null;
+            component.bookUpdated.subscribe((value) => {
+                bookReceived = value;
+            });
+
+            const newValues = {
+                author: "Phillip K Dick",
+                title: "Ubik",
+                year: 1970
+            };
+
+            editInput(getTitleInput(), newValues.title);
+            editInput(getAuthorInput(), newValues.author);
+            editInput(getYearInput(), `${newValues.year}`);
+
+            expect(bookReceived).toBeNull();
+
+            clickSave();
+            expect(bookReceived).not.toBeNull();
+        });
     });
-  });
 });
--- src/app/books/services/books.service.spec.ts
+++ src/app/books/services/books.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { BooksService } from './books.service';
 
-describe('BooksService', () => {
+xdescribe('BooksService', () => {
   let service: BooksService;
 
   beforeEach(() => {

2e271c70184467663cd0800f86399d86da81870e
Maciej Malecki <maciej.malecki@capgemini.com>
2021-10-27 09:51:58 +0200

    async pipe

--- src/app/books/components/book-list/book-list.component.ts
+++ src/app/books/components/book-list/book-list.component.ts
@@ -1,29 +1,21 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Book} from "../../model/book";
 import {BooksService} from "../../services/books.service";
-import {Subscription} from "rxjs";
+import {Observable} from "rxjs";
 
 @Component({
     selector: 'app-book-list',
     templateUrl: './book-list.component.html',
     styleUrls: ['./book-list.component.scss'],
 })
-export class BookListComponent implements OnDestroy {
+export class BookListComponent {
 
-    books: Book[] = [];
+    readonly books$: Observable<Book[]>;
 
     selectedBook: Book | null = null;
 
-    private booksSubscription: Subscription;
-
     constructor(private readonly bookService: BooksService) {
-        this.booksSubscription = this.bookService.books$.subscribe(value => {
-           this.books = value;
-        });
-    }
-
-    ngOnDestroy(): void {
-        this.booksSubscription.unsubscribe();
+        this.books$ = this.bookService.books$;
     }
 
     selectBook(book: Book): void {
